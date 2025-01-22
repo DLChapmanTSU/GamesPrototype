c32f2a62b16cec2db54a7100d943ae9eb2f320bb
Daniel Lewis Chapman <A0028242@live.tees.ac.uk>
2025-01-22 20:16:00 +0000

    DanielC | Initial Commit

---
+++ Source/GamesPrototype/GamesPrototype.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GamesPrototype.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GamesPrototype, "GamesPrototype" );
